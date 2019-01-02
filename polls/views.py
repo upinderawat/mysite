20c0970ea22e96251aa3f883d316c650a41bfe48
upizzz <upinderawat@gmail.com>
2019-01-02 23:26:20 +0530

    Added Views and mapping

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse('Polls is working')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse("You're looking at results of Question: %s " % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting question %s" % question_id)
